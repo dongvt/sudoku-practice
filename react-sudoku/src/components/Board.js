22dd6a7c87c82364c67f975af113425abd083bab
dongvt <car16116@byui.edu>
2022-05-18 16:46:39 -0600

    First sodoku deisn ready

--- react-sudoku/src/components/Board.js
+++ react-sudoku/src/components/Board.js
@@ -4,8 +4,10 @@ const Board = (props) => {
     const board = new Array(9*9).fill(null);
     return <div className={styles.board}>
         {board.map((sq,i) => {
-            const idx = i % 9;
-            const oddSq = ~~(idx / 3) % 2 === 1;
+            const col = i % 9;
+            const row = ~~(i / 9);
+            let oddSq = ~~(col / 3) % 2 === 1;
+            if(~~(row / 3) % 2 === 0) oddSq = !oddSq;
             return <Square key={i} odd={oddSq}/>
         })}
     </div>;
--- react-sudoku/src/components/Square.js
+++ react-sudoku/src/components/Square.js
@@ -1,6 +1,8 @@
 import styles from './Square.module.css';
 const Square = (props) => {
-    return <div className={`${styles.square} ${props.odd ? styles.odd : styles.even}`} />
+    return <div className={`${styles.square} ${props.odd ? styles.odd : styles.even}`} >
+        {props.txt}
+        </div>
 }
 
 export default Square;
