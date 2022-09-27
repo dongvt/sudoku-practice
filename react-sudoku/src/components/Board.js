f89ab7d3f0f426a77652e36167e06294228b0a9b
dongvt <car16116@byui.edu>
2022-09-26 22:39:42 -0400

    Added demo board, fixed number validation by box / Added read me to order the project

--- react-sudoku/src/components/Board.js
+++ react-sudoku/src/components/Board.js
@@ -4,6 +4,21 @@ import Square from "./Square";
 import Keys from "./Keys";
 
 import styles from "./Board.module.css";
+
+const BOX = 3;
+const BOARD = 9;
+
+/**
+ * Given a number n return the multiple of three that is closest to n
+ * @param {int} number
+ * @return {int} minMultiple
+ */
+const getMinMultiple = (number) => {
+  number = number + ~~(number / BOX);
+  number = number - (number % BOX);
+  return number;
+}
+
 const Board = (props) => {
 
   const [clickedId, setClickedId] = useState(0);
@@ -20,7 +35,50 @@ const Board = (props) => {
   ]
   const [boardValues, setBoardValues] = useState(sudokuInitialState);
 
+  //Function to test Sudoku
+
+  const validateNumberOnBoard = (idx,currentNumber) => {
+    const col = idx % 9;
+    const row = ~~(idx / 9);
+    //const box = ~~(~~(row/3) * 3 + col / 3); //Box index
+
+    let set = new Set(); //To check repeating values
+
+    //Check same column
+    let colIdx = col;
+    for(let i = 0; i < 9; i++) {
+      set.add(boardValues[colIdx]);
+      colIdx += 9;
+      if(set.has(currentNumber)) return false;
+    }
+
+    //Check same row
+    let rowIdx = row * 9;
+    set.clear();
+    for(let i = 0; i < 9; i++) {
+      set.add(boardValues[rowIdx++]);
+      if(set.has(currentNumber)) return false;
+    }
+
+    //Check box
+    let iRow = getMinMultiple(row);
+    let iCol = getMinMultiple(col); 
+    set.clear();
+    for(let i = iRow ; i < BOX + iRow; i++) {
+      for(let j = iCol; j < BOX + iCol; j++) {
+        //Compute the index by getting the current row and adding current column multiplied by the board size
+        set.add(boardValues[i + BOARD * j]);
+        //console.log(boardValues[i + BOARD * j],i + BOARD * j)
+        if(set.has(currentNumber)) return false;
+      }
+    }
+
+    return true;
+    
+  }
+
   const setBoardNumber = (number) => {
+    console.log(validateNumberOnBoard(clickedId,number));
     setBoardValues((oldBoard) => {
         oldBoard[clickedId] = number;
         return [...oldBoard];
@@ -30,6 +88,7 @@ const Board = (props) => {
   const setClickedBox = (id) => {
     setClickedId(id);
   }
+
   return (
     <Fragment>
       <div className={styles.board}>
